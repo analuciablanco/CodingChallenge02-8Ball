54ce84da681d2d2465540657cf25d8dc4115fdb4
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-10-01 13:11:44 -0700

    Initial Commit

---
+++ 8BallApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8BallApp
+//
+//  Created by Ana Lucia Blanco on 10/1/19.
+//  Copyright © 2019 Ana Lucia Blanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
