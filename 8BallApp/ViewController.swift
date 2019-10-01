7754e550e5b631e63e874ba5cbc7fe30033a7c87
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-10-01 13:48:34 -0700

    First commit

--- 8BallApp/ViewController.swift
+++ 8BallApp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0...4)
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
 }
 
